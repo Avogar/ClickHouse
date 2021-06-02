f4a0e4db7a60ada2af34b5c12a19af254d541fe8
Igor Baliuk <baliuk.igor@gmail.com>
2021-06-02 04:14:01 +0200

    Fix conflict-resolving issues

--- src/Formats/FormatFactory.cpp
+++ src/Formats/FormatFactory.cpp
@@ -200,7 +200,7 @@ InputFormatPtr FormatFactory::getInput(
     const String & name,
     ReadBuffer & buf,
     IInputFormatHeader & format_header,
-    const Context & context,
+    ContextConstPtr context,
     UInt64 max_block_size,
     const std::optional<FormatSettings> & format_settings) const
 {
@@ -318,7 +318,7 @@ InputFormatPtr FormatFactory::getInputFormat(
     const String & name,
     ReadBuffer & buf,
     IInputFormatHeader & format_header,
-    const Context & context,
+    ContextConstPtr context,
     UInt64 max_block_size,
     const std::optional<FormatSettings> & _format_settings) const
 {
@@ -326,7 +326,7 @@ InputFormatPtr FormatFactory::getInputFormat(
     if (!input_getter)
         throw Exception("Format " + name + " is not suitable for input with format header", ErrorCodes::FORMAT_IS_NOT_SUITABLE_FOR_INPUT);
 
-    const Settings & settings = context.getSettingsRef();
+    const Settings & settings = context->getSettingsRef();
 
     auto format_settings = _format_settings
         ? *_format_settings : getFormatSettings(context);
@@ -345,7 +345,7 @@ InputFormatPtr FormatFactory::getInputFormat(
 InputFormatHeaderPtr FormatFactory::getInputFormatHeader(
     const String & name,
     ReadBuffer & buf,
-    const Context & context,
+    ContextConstPtr context,
     UInt64 max_block_size,
     const std::optional<FormatSettings> & _format_settings) const
 {
@@ -353,7 +353,7 @@ InputFormatHeaderPtr FormatFactory::getInputFormatHeader(
     if (!format_header_getter)
         throw Exception("Format header " + name + " is not registered", ErrorCodes::LOGICAL_ERROR);
 
-    const Settings & settings = context.getSettingsRef();
+    const Settings & settings = context->getSettingsRef();
 
     auto format_settings = _format_settings
         ? *_format_settings : getFormatSettings(context);
